fed415fe0688b90741ace7ee299d211a8726c36d
Prayerr <k.zyuz@yandex.ru>
2024-03-02 17:05:22 +0300

    added .env

--- src/pool.js
+++ src/pool.js
@@ -1,12 +1,15 @@
 import pkg from "pg";
+import dotenv from "dotenv";
 const { Pool } = pkg;
 
+dotenv.config();
+
 const pool = new Pool({
-  user: "postgres",
-  password: "root",
-  host: "localhost",
-  port: 5432,
-  database: "coursework",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 export default pool;
