3499427dc423c79dbecaa5af50b9c89807a2ca4c
Prayerr <k.zyuz@yandex.ru>
2024-03-18 23:32:26 +0300

    Renaming

--- db/Inserts_exports/exports.sql
+++ db/Inserts_exports/exports.sql
@@ -1,3 +1,3 @@
 COPY restaurant TO '/path/to/restaurant.csv' DELIMITER ',' CSV HEADER;
 
-pg_dump -U postgres -d courseworkpg > E:/Projects/coursework/backups/backupDBPG.sql
+pg_dump -U postgres -d courseworkpg > E:/Projects/coursework/backups/dumpDB.sql
